3ec580d1a8d369937075aae547b54039e1b0b7f2
Manolo <manolovp94@gmail.com>
2026-03-23 18:56:28 -0600

    feat(dashboard): Add new modulo to manage the dashboard information

---
+++ src/main/java/mx/gob/cimientosdelrenacimiento/CimientosDelRenacimientoBackend/dashboard/service/DashboardServiceImpl.java
@@ -0,0 +1,45 @@
+package mx.gob.cimientosdelrenacimiento.CimientosDelRenacimientoBackend.dashboard.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import mx.gob.cimientosdelrenacimiento.CimientosDelRenacimientoBackend.dashboard.dto.DashboardStatsDTO;
+import mx.gob.cimientosdelrenacimiento.CimientosDelRenacimientoBackend.dashboard.mapper.DashboardMapper;
+import mx.gob.cimientosdelrenacimiento.CimientosDelRenacimientoBackend.obra.repository.ObraRespository;
+import mx.gob.cimientosdelrenacimiento.CimientosDelRenacimientoBackend.util.StringNormalizer;
+
+@Service
+@RequiredArgsConstructor
+public class DashboardServiceImpl implements IDashboardService {
+
+    private final DashboardMapper dashboardMapper;
+    private final ObraRespository obraRespository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public DashboardStatsDTO getStats() {
+
+        List<Object[]> mainRaw = obraRespository.countObrasByMunicipality();
+
+        // Normalizamos y agrupamos manualmente los municipios
+        Map<String, Long> normalizedMuni = mainRaw.stream()
+        .collect(Collectors.groupingBy(
+            row -> StringNormalizer.normalizer(row[0].toString()),
+            Collectors.summingLong(row -> (Long) row[1])
+        ));
+
+        return DashboardStatsDTO.builder()
+            .totalObras(obraRespository.count())
+            .totalInvestment(obraRespository.sumAllInvestment())
+            .averageProgress(obraRespository.getAverageProgress() != null ? obraRespository.getAverageProgress() : 0.0)
+            .countByStatus(dashboardMapper.toCountMap(obraRespository.countObrasByStatus()))
+            .countByMunicipality(normalizedMuni)
+            .build();
+    }
+
+}
---
+++ src/main/java/mx/gob/cimientosdelrenacimiento/CimientosDelRenacimientoBackend/dashboard/service/IDashboardService.java
@@ -0,0 +1,8 @@
+package mx.gob.cimientosdelrenacimiento.CimientosDelRenacimientoBackend.dashboard.service;
+
+import mx.gob.cimientosdelrenacimiento.CimientosDelRenacimientoBackend.dashboard.dto.DashboardStatsDTO;
+
+public interface IDashboardService {
+
+    DashboardStatsDTO getStats();
+}
