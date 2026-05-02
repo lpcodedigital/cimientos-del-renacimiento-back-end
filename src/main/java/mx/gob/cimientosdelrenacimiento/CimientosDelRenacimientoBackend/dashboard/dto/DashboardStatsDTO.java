68637bc630b4fdf0504b0d8174ade258d5ec4e2c
Manolo <manolovp94@gmail.com>
2026-05-01 21:33:33 -0600

    feat(curso-obra-user): Implement logic to display new KPIs

--- src/main/java/mx/gob/cimientosdelrenacimiento/CimientosDelRenacimientoBackend/dashboard/dto/DashboardStatsDTO.java
+++ src/main/java/mx/gob/cimientosdelrenacimiento/CimientosDelRenacimientoBackend/dashboard/dto/DashboardStatsDTO.java
@@ -16,5 +16,17 @@ public class DashboardStatsDTO {
     // Estos Maps contienen el "nombre del grupo" -> conteo
     private Map<String, Long> countByStatus; // Función: Conteo para gráfica de pastel/estatus
     private Map<String, Long> countByMunicipality; // Función: Conteo para gráfica de barras/geografíco
+    
+    private long municipalitiesWithObras; // KPI de Cobertura
+    private Map<String, Long> countByAgency; // KPI de Ejecutoras
 
+    // Usuarios
+    private long totalUsers;
+    private long activeUsers;
+
+    // Cursos
+    private long totalCursos;
+
+    // Ejecutoras
+    private long totalAgency;
 }
